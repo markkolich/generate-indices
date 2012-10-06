ba4861bed8457796d94289f9ced39e8d49f8650d
Mark Kolich <social@kolich.com>
2012-10-06 12:22:58 -0700

    cleaning up GenerateIndices.scala a bit -- doing the right thing with args

--- src/main/scala/GenerateIndices.scala
+++ src/main/scala/GenerateIndices.scala
@@ -1,28 +1,35 @@
 import java.io._
 import scala.xml._
 
-object GenerateIndices extends Application {
-  val root = new File("../repo")
-  recurse(root)
+object GenerateIndices {
 
-  def recurse(file: File): Unit = {
-    if (file.isDirectory) {
-      write(file, generateIndex(file))
-    }
-    file.listFiles().filter(_.isDirectory()).foreach { f =>
-      recurse(f)
+  def main(args: Array[String]) {
+    if(args.length<1) {
+      println("Usage: generate-indices.jar [root dir name]")
+    } else {
+      val root = new File(args(0))
+      recurse(root, root)
     }
   }
 
-  def generateIndex(dir: File) = {
+  def recurse(root: File, file: File): Unit = {
+    if (file.isDirectory) {
+      write(file, generateIndex(root, file))
+    }
+    file.listFiles().filter(_.isDirectory()).foreach { f =>
+      recurse(root, f)
+    }
+  }
+
+  def generateIndex(root: File, dir: File) = {
     val dirs = dir.listFiles.filter(_.isDirectory).map(_.getName + "/").toList.sorted
     val files = dir.listFiles.filter(f => f.isFile && f.getName != "index.html").map(_.getName).toList.sorted
-    // Using an embedded stylesheet in an XML literal, a double curly brace
+    // When using an embedded stylesheet in an XML literal, a double curly brace
     // "{{" or "}}" is the escape pattern mojo to actually get a curly brace.
     <html>
     <head><style type="text/css">body{{font-family:sans-serif;font-size:20px;color:#B78E68;}}h1{{font-size:30px;}}ul{{list-style:none;padding-left:20px;}}a:visited{{color:blue;}}</style></head>
     <body>
-      <h1>{ path(dir) }</h1>
+      <h1>{ path(root, dir) }</h1>
       <ul>
         <li><a href="../">..</a></li>
         { for (f <- dirs ::: files) yield <li><a href={ f }>{ f }</a></li> }
@@ -31,11 +38,7 @@ object GenerateIndices extends Application {
     </html>
   }
 
-  def path(dir: File): String = if (dir == root) "repo" else path(dir.getParentFile) + "/" + dir.getName
-
-  def rootLink(dir: File): String = {
-    if (dir == root) ".." else rootLink(dir.getParentFile) + "/.."
-  }
+  def path(root: File, dir: File): String = if (dir == root) root.getName else path(root, dir.getParentFile) + "/" + dir.getName
 
   def write(dir: File, n: Node) {
     val out = new FileWriter(new File(dir, "index.html"))
@@ -43,5 +46,6 @@ object GenerateIndices extends Application {
     out.write(new PrettyPrinter(120,2).format(n))
     out.close
   }
+
 }
 
