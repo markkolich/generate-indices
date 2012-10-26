022e78b7f0307a1df332bfffc4fbbf47f07ce559
Mark Kolich <social@kolich.com>
2012-10-26 14:41:05 -0700

    code cleanup and a new README

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-generate-indices
-================
+# generate-indices
 
-Generate HTML indices (index.html pages) for a set of nested directories.
+Trivial Scala project to generate HTML indices (index.html pages) for a set of nested directories.
+
+Meh.
+
+## Using
+
+Download the latest runnable JAR from the <a href="https://github.com/markkolich/generate-indices/downloads">download page</a>.
+
+The JAR is self-contained courtesy of the <a href="https://github.com/sbt/sbt-onejar">sbt-onejar</a> plugin.  Although the project is written in Scala, you don't need Scala to run it -- just a working JVM.
+
+Let's say you want to generate index.html pages for all files and directories under directory **foobar**, then run ...
+
+    #~/> java -jar generate-indices-0.2-one-jar.jar foobar
+
+## Building 
+
+This project is built using <a href="https://github.com/harrah/xsbt">SBT (the Simple Build Tool)</a>.
+
+To clone and build generate-indices, you must have <a href="http://www.scala-sbt.org/release/docs/Getting-Started/Setup">SBT installed and configured on your computer</a>.
+
+To build, clone the repository.
+
+    #~> git clone git://github.com/markkolich/generate-indices.git
+
+Run SBT from within generate-indices.
+
+    #~> cd generate-indices
+    #~/generate-indices> sbt
+    ...
+    >
+
+You will see an SBT prompt once all dependencies are resolved and the project is loaded.
+
+In SBT, run `one-jar` to compile and package the self-contained runnable JAR.
+
+    > one-jar
+    ...
+    [info] Compiling 1 Scala source to ~/generate-indices/target/classes...
+    [info] Packaging ~/generate-indices/target/generate-indices-0.2.jar ...
+    [info] Done packaging.
+    [info] Packaging ~/generate-indices/target/generate-indices-0.2-one-jar.jar ...
+    [info] Done packaging.
+    [success] Total time: 9 s
+
+Note the resulting one-jar enabled JAR is placed into the **generate-indices/target** directory.
+
+## Licensing
+
+Copyright (c) 2012 <a href="http://mark.koli.ch">Mark S. Kolich</a>
+
+All code in this project is freely available for use and redistribution under the <a href="http://opensource.org/comment/991">MIT License</a>.
+
+See <a href="https://github.com/markkolich/generate-indices/blob/master/LICENSE">LICENSE</a> for details.
--- src/main/scala/GenerateIndices.scala
+++ src/main/scala/GenerateIndices.scala
@@ -1,6 +1,8 @@
 import java.io._
 import scala.xml._
 
+import scala.xml.Unparsed
+
 object GenerateIndices {
 
   def main(args: Array[String]) {
@@ -12,7 +14,7 @@ object GenerateIndices {
     }
   }
 
-  def recurse(root: File, file: File): Unit = {
+  private def recurse(root: File, file: File): Unit = {
     if (file.isDirectory) {
       write(file, generateIndex(root, file))
     }
@@ -21,28 +23,28 @@ object GenerateIndices {
     }
   }
 
-  def generateIndex(root: File, dir: File) = {
+  private def generateIndex(root: File, dir: File) = {
     val dirs = dir.listFiles.filter(_.isDirectory).map(_.getName + "/").toList.sorted
     val files = dir.listFiles.filter(f => f.isFile && f.getName != "index.html").map(_.getName).toList.sorted
     // When using an embedded stylesheet in an XML literal, a double curly brace
     // "{{" or "}}" is the escape pattern mojo to actually get a curly brace.
     <html>
-    <head><style type="text/css">body{{font-family:sans-serif;font-size:20px;color:#B78E68;}}h1{{font-size:30px;}}ul{{list-style:none;padding-left:20px;}}a:visited{{color:blue;}}</style></head>
+    <head><style type="text/css">body{{font-family:sans-serif;font-size:20px;color:#B78E68;}}hr{{color:#ccc;background-color:#ccc;border:0;height:1px;}}h1{{font-size:30px;}}.footer{{color:#B78E68;font-size:12px;}}.footer a{{color:#B78E68 !important;}}ul{{list-style:none;padding-left:20px;}}a:visited{{color:blue;}}</style></head>
     <body>
       <h1>{ path(root, dir) }</h1>
       <ul>
         <li><a href="../">..</a></li>
         { for (f <- dirs ::: files) yield <li><a href={ f }>{ f }</a></li> }
       </ul>
-      <hr/>
-      Generated using <a href="https://github.com/markkolich/generate-indices">generate-indices</a>.
+      {Unparsed("<hr/>")}
+      <p class="footer">Generated using <a href="http://github.com/markkolich/generate-indices">generate-indices</a></p>
     </body>
     </html>
   }
 
-  def path(root: File, dir: File): String = if (dir == root) root.getName else path(root, dir.getParentFile) + "/" + dir.getName
+  private def path(root: File, dir: File): String = if (dir == root) root.getName else path(root, dir.getParentFile) + "/" + dir.getName
 
-  def write(dir: File, n: Node) {
+  private def write(dir: File, n: Node) {
     val out = new FileWriter(new File(dir, "index.html"))
     out.write("""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">""")
     out.write(new PrettyPrinter(120,2).format(n))
